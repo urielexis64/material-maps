941bf1c2f57707b00eb09576671150c88da1a04c
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-28 21:47:14 -0700

    add, remove, removeAll and localStorage

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Marker } from '../../classes/marker.class';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MarkerDialogComponent } from './marker-dialog/marker-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-map',
@@ -11,16 +14,71 @@ export class MapComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() {
-    const newMarker = new Marker(51.678418, 7.809007);
-    const newMarker2 = new Marker(51.778418, 7.709007);
-    this.markers.push(newMarker);
-    this.markers.push(newMarker2);
+  constructor(private _snackBar: MatSnackBar, public dialog: MatDialog) {
+    this.loadStorage();
   }
 
   ngOnInit(): void {}
 
   addMarker(event) {
-    console.log(event);
+    const { lat, lng } = event.coords;
+    const newMarker = new Marker(lat, lng);
+    this.markers.push(newMarker);
+    this.saveStorage();
+    this.openSnackBar('Marker added!', 'CLOSE');
+  }
+
+  removeMarker(index: number) {
+    const removed = this.markers.splice(index, 1);
+    this.saveStorage();
+    this.openSnackBar('Marker removed!', 'UNDO', removed);
+  }
+
+  removeAllMarkers() {
+    this.markers = [];
+    this.saveStorage();
+  }
+
+  openSnackBar(message: string, action: string, data?: Marker[]) {
+    this._snackBar
+      .open(message, action, {
+        duration: 2000,
+        data,
+      })
+      .onAction()
+      .subscribe(() => {
+        if (data) {
+          this.markers.push(...data);
+        }
+      });
+  }
+
+  openDialog(marker: Marker): void {
+    const dialogRef = this.dialog.open(MarkerDialogComponent, {
+      width: '250px',
+      data: {
+        title: marker.title,
+        desc: marker.desc,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      if (result) {
+        marker.title = result.title;
+        marker.desc = result.desc;
+        this.saveStorage();
+        this.openSnackBar('Saved changes!', 'CLOSE');
+      }
+    });
+  }
+
+  saveStorage() {
+    localStorage.setItem('markers', JSON.stringify(this.markers));
+  }
+
+  loadStorage() {
+    const storage = localStorage.getItem('markers');
+    if (storage) this.markers = JSON.parse(storage);
   }
 }
