a330aa950a4df6f06545135474829399a30cbb03
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-28 21:46:32 -0700

    ReactiveForms, MarkersDialog imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { MapComponent } from './components/map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AgmCoreModule } from '@agm/core';
+import { MarkerDialogComponent } from './components/map/marker-dialog/marker-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  entryComponents: [MarkerDialogComponent],
+  declarations: [AppComponent, MapComponent, MarkerDialogComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
-    CommonModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBODpHy9-3xzW4X6uol7lSjGp8NQIqW60I',
     }),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
